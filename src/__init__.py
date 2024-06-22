ee22abc56a9bf449476ffcd48d6ceb748c3791ab
Bikatr7 <Bikatr7@proton.me>
2024-06-21 23:35:34 -0600

    added initial files 0.0.1-alpha

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+## Copyright Kakusui LLC 2024 (https://kakusui.org) (https://github.com/Kakusui) (https://github.com/Kakusui/Elucidate)
+## Use of this source code is governed by an GNU Lesser General Public License v2.1
+## license that can be found in the LICENSE file.
+
+from .version import VERSION as __version__  # noqa
+
+__author__ = "Bikatr7 <Bikatr7@proton.me>"
---
+++ src/version.py
@@ -0,0 +1,5 @@
+## Copyright Kakusui LLC 2024 (https://kakusui.org) (https://github.com/Kakusui) (https://github.com/Kakusui/Elucidate)
+## Use of this source code is governed by an GNU Lesser General Public License v2.1
+## license that can be found in the LICENSE file.
+
+VERSION = "0.0.1-alpha"
