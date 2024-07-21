2b41d236183356113e4bbb50e5cae7f2d54b7e69
Bikatr7 <Bikatr7@proton.me>
2024-07-21 16:39:44 -0600

    added external imports

--- src/elucidate/__init__.py
+++ src/elucidate/__init__.py
@@ -6,6 +6,47 @@ from .version import VERSION as __version__  # noqa
 
 __author__ = "Kaden Bilyeu (Bikatr7) <Bikatr7@proton.me>"
 
+## EasyTL things
+from easytl import Message
+
+from easytl import MODEL_COSTS, ALLOWED_GEMINI_MODELS, ALLOWED_OPENAI_MODELS, ALLOWED_ANTHROPIC_MODELS, VALID_JSON_OPENAI_MODELS, VALID_JSON_GEMINI_MODELS, VALID_JSON_ANTHROPIC_MODELS, MODEL_MAX_TOKENS
+
+## google generic exception
+from easytl import GoogleAPIError
+
+## openai generic exception
+from easytl import OpenAIError
+
+## anthropic generic exception
+from easytl import AnthropicError
+
+## service specific exceptions
+from easytl import OpenAIAPIError, OpenAIConflictError, OpenAINotFoundError, OpenAIAPIStatusError, OpenAIRateLimitError, OpenAIAPITimeoutError, OpenAIBadRequestError, OpenAIAPIConnectionError, OpenAIAuthenticationError, OpenAIInternalServerError, OpenAIPermissionDeniedError, OpenAIUnprocessableEntityError, OpenAIAPIResponseValidationError
+from easytl import AnthropicAPIError, AnthropicConflictError, AnthropicNotFoundError, AnthropicAPIStatusError, AnthropicRateLimitError, AnthropicAPITimeoutError, AnthropicBadRequestError, AnthropicAPIConnectionError, AnthropicAuthenticationError, AnthropicInternalServerError, AnthropicPermissionDeniedError, AnthropicUnprocessableEntityError, AnthropicAPIResponseValidationError
+
+## Elucidate things
 from .elucidate import Elucidate
 
-__all__ = ["Elucidate"]
+from .util.classes import SystemTranslationMessage, ModelTranslationMessage
+from .util.classes import ChatCompletion
+from .util.classes import GenerateContentResponse, AsyncGenerateContentResponse, GenerationConfig
+from .util.classes import AnthropicMessage, AnthropicTextBlock, AnthropicToolUseBlock
+from .util.classes import NOT_GIVEN, NotGiven
+
+from .exceptions import ElucidateException, InvalidElucidateSettingsException
+
+__all__ = [
+    "Elucidate",
+    "Message", "SystemTranslationMessage", "ModelTranslationMessage",
+    "ChatCompletion",
+    "GenerateContentResponse", "AsyncGenerateContentResponse", "GenerationConfig",
+    "AnthropicMessage","AnthropicTextBlock", "AnthropicToolUseBlock",
+    "NOT_GIVEN","NotGiven",
+    "MODEL_COSTS", "ALLOWED_GEMINI_MODELS", "ALLOWED_OPENAI_MODELS", "ALLOWED_ANTHROPIC_MODELS", "VALID_JSON_OPENAI_MODELS", "VALID_JSON_GEMINI_MODELS", "VALID_JSON_ANTHROPIC_MODELS", "MODEL_MAX_TOKENS",
+    "GoogleAPIError",
+    "OpenAIError",
+    "AnthropicError",
+    "OpenAIAPIError", "OpenAIConflictError", "OpenAINotFoundError", "OpenAIAPIStatusError", "OpenAIRateLimitError", "OpenAIAPITimeoutError", "OpenAIBadRequestError", "OpenAIAPIConnectionError", "OpenAIAuthenticationError", "OpenAIInternalServerError", "OpenAIPermissionDeniedError", "OpenAIUnprocessableEntityError", "OpenAIAPIResponseValidationError",
+    "AnthropicAPIError", "AnthropicConflictError", "AnthropicNotFoundError", "AnthropicAPIStatusError", "AnthropicRateLimitError", "AnthropicAPITimeoutError", "AnthropicBadRequestError", "AnthropicAPIConnectionError", "AnthropicAuthenticationError", "AnthropicInternalServerError", "AnthropicPermissionDeniedError", "AnthropicUnprocessableEntityError", "AnthropicAPIResponseValidationError",
+    "ElucidateException", "InvalidElucidateSettingsException"
+]
